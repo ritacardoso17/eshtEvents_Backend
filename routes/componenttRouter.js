0a7142c54a3809a6019c8c1009b90e51a280448f
raquelesilva <raquelesilva99@gmail.com>
2020-05-22 17:49:03 +0100

    Components done kinda

--- routes/componenttRouter.js
+++ routes/componenttRouter.js
@@ -3,7 +3,9 @@ var router = Router()
 
 const controller = require("../controllers/Menus/componentt/componentController")
 
-router.post("/components", controller.addComponent)
+router.post("/components", controller.addComponents)
+router.delete("/components/:id", controller.removeComponents)
+router.put("/components/:id", controller.updateComponents)
 
 module.exports = router
 
