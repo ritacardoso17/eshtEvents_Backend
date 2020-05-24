c5cbfcea7e906ca380baa241a29483d0934721ed
Rita Cardoso <rita.cardoso.17@gmail.com>
2020-05-25 00:41:02 +0100

    token wow

---
+++ Middleware.js
@@ -0,0 +1,30 @@
+const jsonwebtoken = require('jsonwebtoken')
+const dbConfig = require('./database/dbConfig.json')
+const config = require("./config.json")
+const mysql = require("mysql")
+var connection = mysql.createConnection(dbConfig)
+
+function verifyToken(req, res, next) {
+    let token = req.headers['x-access-token'] || req.headers['authorization']
+    if (token.startsWith('Bearer ')) {
+        token = token.slice(7, token.length)
+    }
+    const sql = "SELECT token FROM token_bloqueado WHERE token=?"
+    connection.connect()
+    connection.query(sql, [token], function (error, rows, fields) {
+        if (rows.length != 0) { return res.json({ success: false, message: "Token Bloqueado" }) }
+        else if (rows.length == 0) {
+            jsonwebtoken.verify(token, config.secret, (error, decoded) => {
+                if (error) {
+                    return res.json({ success: false, message: "Token Invalido" })
+                } else {
+                    req.decoded = decoded
+                    next()
+                }
+            })
+        }
+    })
+    connection.end()
+}
+
+module.exports = { verifyToken: verifyToken }
--- controllers/Users/userController.js
+++ controllers/Users/userController.js
@@ -1,6 +1,36 @@
 const userFunctions = require("./userFunctions")
 const bcrypt = require("bcrypt")
 
+class verifyLogin {
+    login(req, result) {
+        let pass = req.body.pass
+        let email = req.body.email
+
+        userFunctions.login(pass, email, (error, sucess) => {
+            if (error) {
+                throw error
+                return
+            }
+            result.json(sucess)
+        })
+    }
+    landingPage(result) {
+        result.json({ success: true, message: "eshtEvents" })
+    }
+}
+function logout(req, result) {
+    let token = req.headers['x-access-token'] || req.headers['authorization']
+    if (token.startsWith('Bearer ')) {
+        token = token.slice(7, token.length)
+    }
+    userFunctions.logout(token, (error, sucess) => {
+        if (error) {
+            throw error
+            return
+        }
+        result.json(sucess)
+    })
+}
 function addUser(req, result) {
 
     let name = req.body.name
@@ -12,7 +42,7 @@ function addUser(req, result) {
     let email = req.body.email
 
     bcrypt.hash(pass, 10, function (err, hash) {
-        userFunctions.addUser(name,hash , img, data, telemovel,idE, email, (error, sucess) => {
+        userFunctions.addUser(name, hash, img, data, telemovel, idE, email, (error, sucess) => {
             if (error) {
                 throw error
                 return
@@ -23,4 +53,4 @@ function addUser(req, result) {
 
 
 }
-module.exports = { addUser: addUser }
+module.exports = { addUser: addUser, verifyLogin: verifyLogin,logout:logout }
