1900fc2f8e4ab45654af424ae5dd8ecb24a48cbc
Rita Cardoso <rita.cardoso.17@gmail.com>
2020-06-15 12:50:47 +0100

    aada

--- routes/reservationRouter.js
+++ routes/reservationRouter.js
@@ -5,7 +5,7 @@ var router = Router()
 const controller = require("../controllers/EventReservation/reservation/reservationControllers")
 router.post("/reservations", middleware.verifyToken, controller.addReservations)
 router.delete("/reservations/:id", middleware.verifyToken, controller.removeReservations)
-router.get("/reservations", middleware.verifyToken, controller.getReservations)
+router.get("/reservations", controller.getReservations)
 router.get("/reservations/:id", middleware.verifyToken, controller.getReservationsId)
 
 module.exports = router
