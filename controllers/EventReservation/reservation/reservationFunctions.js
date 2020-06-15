ddb272d8f6840a7226dfb99faedfd549dd3b5eaa
raquelesilva <raquelesilva99@gmail.com>
2020-06-15 18:25:39 +0100

    id_notificacao

--- controllers/EventReservation/reservation/reservationFunctions.js
+++ controllers/EventReservation/reservation/reservationFunctions.js
@@ -30,7 +30,7 @@ function removeReservations(id, callback) {
 //retorna todos os dados da tabela reerva_evento
 function getReservations( callback) {
     connection 
-    const sql = "SELECT id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao, id_notificacao from reserva_evento"
+    const sql = "SELECT id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao from reserva_evento"
     connection.query(sql, function (error, results) {
         if (error) callback(error)
         callback(null, {results })
@@ -40,7 +40,7 @@ function getReservations( callback) {
 //retorna os dados de uma so reserva selecionada por id
 function getReservationsId(id, callback) {
     connection 
-    const sql = "SELECT id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao, id_notificacao from reserva_evento WHERE id_reserva=?"
+    const sql = "SELECT id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao from reserva_evento WHERE id_reserva=?"
     connection.query(sql, [id], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: results })
