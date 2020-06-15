058a5b94381bea1af1bb4dd5295cc1ae1a18afa9
Rita Cardoso <rita.cardoso.17@gmail.com>
2020-06-15 17:58:36 +0100

    dadasd

--- routes/rentsRouter.js
+++ routes/rentsRouter.js
@@ -5,7 +5,7 @@ var router = Router()
 const controller = require("../controllers/Rooms/rent/rentsControllers")
 router.post("/roomRents", middleware.verifyToken, controller.addRents)
 router.delete("/roomRents/:id", middleware.verifyToken, controller.removeRents)
-router.get("/roomRents", middleware.verifyToken, controller.getRents)
+router.get("/roomRents", controller.getRents)
 router.get("/roomRents/:id", middleware.verifyToken, controller.getRentsId)
 
 module.exports = router
