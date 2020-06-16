21f479953d6e12b2a8213e837ff45d7f11a31cf1
Rita Cardoso <rita.cardoso.17@gmail.com>
2020-06-16 15:16:09 +0100

    asdasdasda

--- controllers/EventReservation/reservation/reservationFunctions.js
+++ controllers/EventReservation/reservation/reservationFunctions.js
@@ -30,7 +30,7 @@ function removeReservations(id, callback) {
 //retorna todos os dados da tabela reerva_evento
 function getReservations( callback) {
     connection 
-    const sql = "SELECT id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao from reserva_evento"
+    const sql = "SELECT id_reserva,id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao from reserva_evento"
     connection.query(sql, function (error, results) {
         if (error) callback(error)
         callback(null, {results })
