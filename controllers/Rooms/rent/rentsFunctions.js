4a393d33a63bfe18d10d931649c99da8ae53dc9c
Rita Cardoso <rita.cardoso.17@gmail.com>
2020-06-18 17:13:29 +0100

    wfaekmlfaekmnlvfknsvdk

--- controllers/Rooms/rent/rentsFunctions.js
+++ controllers/Rooms/rent/rentsFunctions.js
@@ -10,7 +10,7 @@ function addRents(id_user, date_reserv, date_required, duration, id_room, reason
     const sql = "INSERT INTO aluguer_espaco (id_utilizador, data_hora_aluguer, data_hora_requirida, duracao, id_estado, id_espaco, motivo, opiniao) VALUES (?,?,?,?,?,?,?,?)"
     connection.query(sql, [id_user, date_reserv, date_required, duration, 1, id_room, reason, opinion], function (error, results) {
         if (error) callback(error)
-        callback(null, { sucess: true, message: "Aluguer de Espaço Adicionado" })
+        callback(null, { sucess: true, message: results })
     })
 
     connection 
