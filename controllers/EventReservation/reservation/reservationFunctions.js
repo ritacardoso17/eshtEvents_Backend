152dcc97c3f37499cac0647865d04dbc5c87731a
Rita Cardoso <rita.cardoso.17@gmail.com>
2020-06-18 22:33:20 +0100

    fsdfsdfsdf

--- controllers/EventReservation/reservation/reservationFunctions.js
+++ controllers/EventReservation/reservation/reservationFunctions.js
@@ -4,20 +4,20 @@ var connection = mySql.createConnection({ host: process.env.host, user: process.
 // var connection = mySql.createConnection(dbConfig)
 
 //funcao criada para adicionar uma reserva a base de dados
-function addReservations(id_extra, id_user, n_people, date_reserv, date_required, id_uniform, id_reservType, id_menu, id_local, id_decoration, callback) {
-    connection 
+function addReservations(id_extra, id_user, n_people, date_reserv, date_required, id_uniform, id_reservType, id_menu, id_local, id_decoration, obs, callback) {
+    connection
 
-    const sql = "INSERT INTO reserva_evento (id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva, id_estado, id_menu, id_localizacao, id_decoracao) VALUES (?,?,?,?,?,?,?,?,?,?,?)"
-    connection.query(sql, [id_extra, id_user, n_people, date_reserv, date_required, id_uniform, id_reservType, 1, id_menu, id_local, id_decoration, ""], function (error, results) {
+    const sql = "INSERT INTO reserva_evento (id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva, id_estado, id_menu, id_localizacao, id_decoracao,opiniao,observacao) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+    connection.query(sql, [id_extra, id_user, n_people, date_reserv, date_required, id_uniform, id_reservType, 1, id_menu, id_local, id_decoration, "", obs], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: "Reserva Adicionada" })
     })
 
-    connection 
+    connection
 }
 //funcao criada para remover uma reserva selecionada por id
 function removeReservations(id, callback) {
-    connection 
+    connection
 
     const sql = "DELETE FROM reserva_evento WHERE id_reserva = ?"
     connection.query(sql, [id], function (error, results) {
@@ -25,36 +25,36 @@ function removeReservations(id, callback) {
         callback(null, { sucess: true, message: "Reserva Removida" })
     })
 
-    connection 
+    connection
 }
 //retorna todos os dados da tabela reerva_evento
-function getReservations( callback) {
-    connection 
+function getReservations(callback) {
+    connection
     const sql = "SELECT id_reserva,id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao from reserva_evento"
     connection.query(sql, function (error, results) {
         if (error) callback(error)
-        callback(null, {results })
+        callback(null, { results })
     })
-    connection 
+    connection
 }
 //retorna os dados de uma so reserva selecionada por id
 function getReservationsId(id, callback) {
-    connection 
+    connection
     const sql = "SELECT id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva,id_estado, id_menu, id_localizacao, id_decoracao, opiniao from reserva_evento WHERE id_reserva=?"
     connection.query(sql, [id], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: results })
     })
-    connection 
+    connection
 }
 function getReservationsUserId(id, callback) {
-    connection 
+    connection
     const sql = "SELECT id_reserva,id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, tipo_reserva.descritivo as tipoReserva,estado.descritivo as estado, id_menu, id_localizacao, id_decoracao, opiniao FROM reserva_evento,tipo_reserva,estado WHERE id_utilizador=? AND tipo_reserva.id_tipo_reserva = reserva_evento.id_tipo_reserva AND estado.id_estado = reserva_evento.id_estado "
     connection.query(sql, [id], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: results })
     })
-    connection 
+    connection
 }
 //edita os dados da opinião de uma reserva consoante o seu id
 function updateOpinion(opinion, id, callback) {
