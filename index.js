4e840a6b6a1c18b46d7964f819a967242fe0296d
raquelesilva <raquelesilva99@gmail.com>
2020-06-18 19:02:34 +0100

    ssdfsdfsdfsEOEOEOEOEOEOEdfsdfdsfsdfsdfsdfdfdf

--- index.js
+++ index.js
@@ -18,8 +18,15 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({origin:'*'}));
 
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 app.use(userRouter)
 app.use(extraRouter)
 app.use(uniformRouter)
