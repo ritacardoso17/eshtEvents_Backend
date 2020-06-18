e7db59a0840ace69d69e64063fd5af3cc5a898e3
raquelesilva <raquelesilva99@gmail.com>
2020-06-18 17:40:39 +0100

    decoration

--- controllers/EventReservation/decoration/decorationFunctions.js
+++ controllers/EventReservation/decoration/decorationFunctions.js
@@ -6,7 +6,7 @@ var connection = mySql.createConnection({ host: process.env.host, user: process.
 //retorna dados da tabela decoracao da base de dados
 function getDecoration(id, description, img, callback) {
     connection 
-    const sql = "SELECT descritivo FROM decoracao"
+    const sql = "SELECT id_decoracao, descritivo, img FROM decoracao"
     connection.query(sql, [id, description, img], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: results })
