a272e0866db947ed1f39f647dcb1b7f5e5a3014a
raquelesilva <raquelesilva99@gmail.com>
2020-06-18 20:02:47 +0100

    pls pls pls

--- controllers/EventReservation/reservation/reservationFunctions.js
+++ controllers/EventReservation/reservation/reservationFunctions.js
@@ -8,7 +8,7 @@ function addReservations(id_extra, id_user, n_people, date_reserv, date_required
     connection 
 
     const sql = "INSERT INTO reserva_evento (id_extra, id_utilizador, nr_pessoas, data_hora_reserva, data_hora_evento, id_farda, id_tipo_reserva, id_estado, id_menu, id_localizacao, id_decoracao) VALUES (?,?,?,?,?,?,?,?,?,?,?)"
-    connection.query(sql, [id_extra, id_user, n_people, date_reserv, date_required, id_uniform, id_reservType, 1, id_menu, id_local, id_decoration], function (error, results) {
+    connection.query(sql, [id_extra, id_user, n_people, date_reserv, date_required, id_uniform, id_reservType, 1, id_menu, id_local, id_decoration, ""], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: "Reserva Adicionada" })
     })
