9648ffab8d0a6980288c2c433bf1d5ac9ed1dac9
raquelesilva <raquelesilva99@gmail.com>
2020-06-25 16:57:40 +0100

    rents

--- controllers/Rooms/rent/rentsFunctions.js
+++ controllers/Rooms/rent/rentsFunctions.js
@@ -20,7 +20,7 @@ function removeRents(id, callback) {
 
 // Retorna todos os dados dos alugueres
 function getRents(callback) {
-    const sql = "SELECT id_aluguer,utilizador.nome as user, data_hora_aluguer, data_hora_requirida, duracao, estado.descritivo as estado, tipo_espaco.descritivo as espaco, opiniao FROM aluguer_espaco, estado, utilizador, tipo_espaco"
+    const sql = "SELECT id_aluguer, utilizador.nome as user, data_hora_aluguer, data_hora_requirida, duracao, estado.descritivo as estado, tipo_espaco.descritivo as espaco, motivo, opiniao FROM aluguer_espaco, estado, utilizador, tipo_espaco"
     connection.query(sql, function(error, results){
         if (error) callback(error)
         callback(null, { sucess: true, message: results })
