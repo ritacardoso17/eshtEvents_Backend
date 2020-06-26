fe1bef729176f9d17de66de820f2a2309f78b36e
Rita Cardoso <rita.cardoso.17@gmail.com>
2020-06-26 23:07:59 +0100

    addd

--- controllers/workshops/workshopFunctions.js
+++ controllers/workshops/workshopFunctions.js
@@ -3,7 +3,7 @@ let connection = require('../../database/dbConfig.js')
 // Adiciona os workshops
 function addWorkshops(description, n_vacancies, date_hour, id_local, img, callback) {
     const sql = "INSERT INTO inscricao_workshop (descritivo, nr_vagas, data_hora, locutor, id_localizacao, img) VALUES (?,?,?,?,?,?)"
-    connection.query(sql, [description, n_vacancies, date_hour, id_local,img], function (error, results) {
+    connection.query(sql, [description, n_vacancies, date_hour, id_local, img], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: "Workshop Adicionado" })
     })
@@ -19,9 +19,9 @@ function removeWorkshops(id, callback) {
 }
 
 // Altera certos elementos dos workshops
-function updateWorkshops(id,description, n_vacancies, date_hour, id_local,img, callback) {
+function updateWorkshops(id, description, n_vacancies, date_hour, id_local, img, callback) {
     const sql = "UPDATE inscricao_workshop SET descritivo=?, nr_vagas=?, data_hora=?, id_localizacao=?,img=? WHERE id_workshop=? "
-    connection.query(sql, [description, n_vacancies, date_hour, id_local,img,id], function (error, results) {
+    connection.query(sql, [description, n_vacancies, date_hour, id_local, img, id], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: "Workshop Editado" })
     })
@@ -29,17 +29,17 @@ function updateWorkshops(id,description, n_vacancies, date_hour, id_local,img, c
 
 // Retorna todos os workshops
 function getWorkshops(callback) {
-    const sql = "SELECT id_workshop, inscricao_workshop.nome as nome, nr_vagas, data_hora, locutor, institucao.nome as local, inscricao_workshop.descritivo as info, img FROM inscricao_workshop, institucao WHERE institucao.id_ipp = inscricao_workshop.id_localizacao "
-    connection.query(sql, function(error, results){
+    const sql = "SELECT id_workshop, inscricao_workshop.nome as name, nr_vagas, data_hora, locutor, institucao.nome as local, inscricao_workshop.descritivo as info, img FROM inscricao_workshop, institucao WHERE institucao.id_ipp = inscricao_workshop.id_localizacao "
+    connection.query(sql, function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: results })
     })
 }
 
 // Retorna os workshops escolhidos através do seu id
-function getWorkshopsId( id, callback){
+function getWorkshopsId(id, callback) {
     const sql = "SELECT id_workshop, descritivo, nr_vagas, data_hora, id_localizacao FROM inscricao_workshop WHERE id_workshop=?"
-    connection.query(sql, [id], function(error, results){
+    connection.query(sql, [id], function (error, results) {
         if (error) callback(error)
         callback(null, { sucess: true, message: results })
     })
@@ -48,15 +48,30 @@ function getWorkshopsId( id, callback){
 function addUserWorkshops(id_user, id_workshop, callback) {
     const sql = "INSERT INTO workshop_utilizador (id_utilizador, id_workshop) VALUES (?,?)"
     connection.query(sql, [id_user, id_workshop], function (error, results) {
-        if (error) callback(error)
-        callback(null, { sucess: true, message: "Workshop Adicionado" })
+        if (!error) {
+            const sql1 = "SELECT nr_vagas FROM inscricao_workshop WHERE id_workshop=?"
+            connection.query(sql1, [id_workshop], function (error, rows, fields) {
+                let vagasUpdate = rows[0].nr_vagas - 1
+                if (!error) {
+                    const sql2 = "UPDATE inscricao_workshop SET nr_vagas=? WHERE id_workshop=?"
+                    connection.query(sql2, [vagasUpdate], function (error, results) { 
+                         callback(null, { message: "Inscrito no Workshop" }) })
+                }
+
+            })
+          
+        }
+        else { callback(error) }
+
     })
 }
 
-module.exports = { 
-    addWorkshops:addWorkshops,
-    removeWorkshops:removeWorkshops,
-    updateWorkshops:updateWorkshops,
+
+
+module.exports = {
+    addWorkshops: addWorkshops,
+    removeWorkshops: removeWorkshops,
+    updateWorkshops: updateWorkshops,
     getWorkshops: getWorkshops,
     getWorkshopsId: getWorkshopsId,
     addUserWorkshops: addUserWorkshops
